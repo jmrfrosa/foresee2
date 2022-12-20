20ed297e393fc6f3e62ee6bcd66b0045cf338fca
João Rosa <jmrfrosa92@gmail.com>
2022-12-20 22:56:09 +0000

    ci: build configs

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -4,6 +4,9 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build',
+  },
   plugins: [
     preact(),
     // mkcert()
