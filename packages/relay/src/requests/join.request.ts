ec0fa5c24ba81012af8ada68de43cc2c0ebe5fb6
João Rosa <jmrfrosa92@gmail.com>
2022-12-21 01:57:29 +0000

    ci: configuring relay prod build

--- packages/relay/src/requests/join.request.ts
+++ packages/relay/src/requests/join.request.ts
@@ -1,10 +1,3 @@
-import { SdpTypes } from "../lib/client"
-
-export type JoinRequestType = {
-  type: SdpTypes | string
-  sdp: string
-}
-
 export type BroadcastType = {
   id?: string
   type: 'offer' | 'answer' | 'ice'
