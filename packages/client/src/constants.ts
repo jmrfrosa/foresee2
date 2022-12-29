3720fbf9ae133ceff7006645c328d9b398e62de3
João Rosa <jmrfrosa92@gmail.com>
2022-12-29 19:32:33 +0000

    chore: try to fix ice again

--- packages/client/src/constants.ts
+++ packages/client/src/constants.ts
@@ -7,8 +7,8 @@ export const ICE_CONFIG = { iceServers: [
     urls: 'stun:stun.labs.net:3478',
   },
   {
-    urls: 'turn:openrelay.metered.ca:80',
-    credential: 'openrelayserver',
-    username: 'openrelayserver',
+    urls: ['turn:relay.metered.ca:80', 'turn:relay.metered.ca:443'],
+    username: import.meta.env.VITE_OPEN_RELAY_USERNAME,
+    credential: import.meta.env.VITE_OPEN_RELAY_PASSWORD,
   }
 ]}
