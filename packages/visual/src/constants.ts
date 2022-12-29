2d0f088ceb1edbe114f464f5bd1c46e7e6430171
João Rosa <jmrfrosa92@gmail.com>
2022-12-29 19:13:31 +0000

    fix: ice config

--- packages/visual/src/constants.ts
+++ packages/visual/src/constants.ts
@@ -1,2 +1,14 @@
 export const RELAY_URL = import.meta.env.VITE_RELAY_URL
-export const ICE_CONFIG = { iceServers: [{ urls: 'stun:stun.stunprotocol.org:3478' }]}
+export const ICE_CONFIG = { iceServers: [
+  {
+    urls: 'stun:stun.stunprotocol.org:3478',
+  },
+  {
+    urls: 'stun:stun.labs.net:3478',
+  },
+  {
+    urls: 'turn:openrelay.metered.ca:80',
+    credential: 'openrelayserver',
+    username: 'openrelayserver',
+  }
+]}
