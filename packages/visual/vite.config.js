f3d50380f6131325b86ca4e68ecc89deca75fb97
João Rosa <jmrfrosa92@gmail.com>
2022-12-30 16:38:03 +0000

    fix: attempt to fix broken prod build

--- packages/visual/vite.config.js
+++ packages/visual/vite.config.js
@@ -1,7 +1,14 @@
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
     outDir: 'build',
+    rollupOptions: {
+      input: {
+        mainApp: fileURLToPath(new URL('./index.html', import.meta.url)),
+        controlPanel: fileURLToPath(new URL('./controls.html', import.meta.url)),
+      }
+    }
   }
 })
